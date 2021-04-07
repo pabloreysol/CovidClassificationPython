936ff0b6466fd6b672f3546d1442434b5c593963
Pablo Reyes <57462002+pabloreysol@users.noreply.github.com>
2021-04-07 00:38:30 -0500

    Update CovidClassification.py

--- CovidClassification.py
+++ CovidClassification.py
@@ -20,9 +20,9 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
 
-df.drop('Unnamed: 0',axis=1,inplace=True)
+
 st.subheader('Data information: ')
-st.dataframe(df)
+st.dataframe(df.iloc[:,1:])
 st.write(df.describe())
 
 chart = st.bar_chart(df)
